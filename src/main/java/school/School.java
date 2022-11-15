ddd907d7f3eab3e55b8c15386c885833991ecf8b
simon <simon@dancingcloudservices.com>
2022-11-15 13:15:33 -0700

    Day 1 14:15 lab...

--- src/main/java/school/School.java
+++ src/main/java/school/School.java
@@ -2,8 +2,17 @@ package school;
 
 public class School {
   public static void main(String[] args) {
-    Student fred = new Student("Fred", 3.2);
+    Student fred = null;
+//    System.out.println(fred.gpa);
+    fred = new Student("Fred", 3.2);
     fred.name = "";
     fred.gpa = 100; // changes creating invalid state!!!
+
+    try {
+      Student jim = new Student("", 0.1);
+    } catch (IllegalArgumentException ie) {
+      System.out.println("Ooops, that doesn't seem to work");
+    }
+    System.out.println("Program still running!");
   }
 }
--- src/main/java/school/Student.java
+++ src/main/java/school/Student.java
@@ -11,8 +11,27 @@ public class Student {
   double gpa;
 
   public Student(String name, double gpa) {
-    this.name = name;
-    this.gpa = gpa;
+//    if (!isValid(name, gpa)) // logical inverse
+    if (isValid(name, gpa)) {
+      this.name = name;
+      this.gpa = gpa;
+    } else {
+//      this.name = "unknown";
+//      this.gpa = 0.0; // unnecessary!
+      throw new IllegalArgumentException("Bad name or gpa for Student");
+    }
+  }
+
+  // expressions of Object type (anything except the
+  // 8 primitives:
+  // boolean byte short char int long float double
+  // are ALWAYS "references" (very similar to pointers)
+  // they tell you where to find the data
+  // we have the special "where" value null -- which
+  // indicates "there is no value"
+  public static boolean isValid(String name, double gpa) {
+    return gpa >= 0.0 && gpa <= 5.0
+        && name != null && name.length() > 0;
   }
 
 /*  public static void main(String[] args) {
@@ -32,3 +51,17 @@ class Oddball {
     System.out.println("Hello ");
   }
 }*/
+
+/*
+1) create validity check for your student
+2) create a constructor that checks validity
+3) build enough code to verify that a valid student is made,
+   and is initialized with good contents
+4) build more code, so as to verify that an INVALID student
+   blows up.
+For those more comfortable.
+5) add a try/catch structure to print a polite message about
+   the invalid student and then carry on with execution
+6) add methods to update the name, and the gpa (separate methods)
+   and similarly validate the proposed change.
+ */
