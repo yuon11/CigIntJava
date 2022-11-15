c12e86b28cde9641de7853f88672f0416dff9d09
simon <simon@dancingcloudservices.com>
2022-11-15 13:17:34 -0700

    Day 1 14:15 lab...

--- src/main/java/school/School.java
+++ src/main/java/school/School.java
@@ -4,6 +4,7 @@ public class School {
   public static void main(String[] args) {
     Student fred = null;
 //    System.out.println(fred.gpa);
+//    new Student();
     fred = new Student("Fred", 3.2);
     fred.name = "";
     fred.gpa = 100; // changes creating invalid state!!!
